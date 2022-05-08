92f1f6d694d4e08ef3f7c8a9d0921ffe9268aafe
amita <amitasher4@gmail.com>
2022-05-08 21:41:38 +0300

    project full

---
+++ src/api/WeatherAPI.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+const BASE_URL = 'https://dataservice.accuweather.com';
+const API_KEY = '4x0wFtVYvGqcsciZNrDwEye6ZE7gQkQW';
+
+// 6Pe9mKdov22Y2CwNAkHF3wVryFkoq6lf
+
+export const getSuggestions = (searchText) => {
+	return axios.get(`${BASE_URL}/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${searchText}`, {
+		headers: { Accept: 'application/json' }
+	});
+};
+
+export const getCurrentWeather = (id) => {
+	return axios.get(`${BASE_URL}/currentconditions/v1/${id}?apikey=${API_KEY}`, {
+		headers: { Accept: 'application/json' }
+	});
+};
+
+export const getForcast = (id, metric = true) => {
+	return axios.get(`${BASE_URL}/forecasts/v1/daily/5day/${id}?apikey=${API_KEY}&metric=${metric}`, {
+		headers: { Accept: 'application/json' }
+	});
+};
+
+
+
+
---
+++ src/helpers/AutoCompleteText.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './AutoCompleteText.css';
+
+const AutoCompleteText = (props) => {
+ 
+ const renderSuggestions = () => {
+    const { suggestions } = props;
+    if(suggestions.length === 0 ){
+      return null;
+    }
+    return (
+      <ul>
+        {suggestions.map((item) => <li onClick={() => props.onTextClicked(item.LocalizedName)} key={item.Key}>{item.LocalizedName}, {item.Country.LocalizedName} ({item.Country.ID})</li>)}
+      </ul>
+    )
+  }
+
+  
+  
+    return (
+      <div className='AutoCompleteText'>
+        <input
+          type='text'
+          onChange = {props.onTextChanged}
+          value={props.searchText}
+        />
+        {renderSuggestions()}
+      </div>
+    )
+  
+}
+
+export default AutoCompleteText
